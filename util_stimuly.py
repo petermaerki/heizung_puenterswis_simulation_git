dd11925a5d15909786101519980465c700c55c5c
peter.maerki <git.peter@ergoinfo.ch>
2023-05-25 00:15:10 +0200

    .

--- util_stimuly.py
+++ util_stimuly.py
@@ -9,6 +9,18 @@ DAY_S = 24 * 3600.0
 
 class StimuliWintertag:
     def __init__(self):
+        self.umgebungstemperatur_C = -10.0
+        self.start_s = -2 * DAY_S
+        self.duration_s = 5 * DAY_S
+        self.timestep_s = 5 * 60.0
+
+    def do_plot(self, time_s) -> bool:
+        return True
+        # return time_s > 0
+
+
+class StimuliFruehlingstag:
+    def __init__(self):
         self.umgebungstemperatur_C = 15.0
         self.start_s = -2 * DAY_S
         self.duration_s = 5 * DAY_S
