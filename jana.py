c066d3c050fca8aa8590a89c7a2cc70db96cfe6e
peter.maerki <git.peter@ergoinfo.ch>
2023-05-31 08:00:39 +0200

    .

--- jana.py
+++ jana.py
@@ -1,12 +1,3 @@
-jana = 5
-peter = 7
-print(jana * peter)
+liste = [3, 5, 6]
 
-if peter > 5:
-    print("Salamibrot")
-
-
-for salamibrot in range(10):
-    print(f"Salamibrot hat einen Wert von {salamibrot}")
-    if salamibrot > 5:
-        print(f"Salamibrot ist grösser als 5")
+print(liste[1::])
